691c00a5b8178c07a3a44d97df51be59189f2dc6
Roberto Salicio <roberto.salicio@gmail.com>
2008-08-25 11:42:56 +0200

    Avoided the error when there is no a subject by default.

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,20 +1,20 @@
 class UserMailer < ActionMailer::Base
   def signup_notification(user)
     setup_email(user)
-    @subject    += 'Please activate your account'
+    @subject     = (@subject || "") + 'Please activate your account'
     @body[:url]  = activate_url(user.activation_code)
 
   end
 
   def activation(user)
     setup_email(user)
-    @subject    += 'Your account has been activated!'
+    @subject     = (@subject || "") + 'Your account has been activated!'
     @body[:url]  = root_url
   end
 
   def reset_notification(user)
     setup_email(user)
-    @subject    += 'Reset your password'
+    @subject     = (@subject || "") + 'Reset your password'
     @body[:url]  = reset_url(user.password_reset_code)
   end
 
