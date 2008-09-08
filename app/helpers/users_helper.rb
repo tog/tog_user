47e3acce750dd9b5660ffd12e54e4a2444a6ccec
Aitor García <aitor.garcia@gmail.com>
2008-09-08 20:59:55 +0200

    last_users clenaup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,15 +1,7 @@
 module UsersHelper
   
   def last_users(limit=16)
-    users = User.find(:all,
-                      :conditions => ["state = ?", 'active'],
-                      :limit => limit,
-                      :order => 'created_at desc')
-    return if users.empty?
-
-    users.each do |user|
-      yield user
-    end
+    User.find(:all,:conditions => ["state = ?", 'active'],:limit => limit,:order => 'created_at desc')
   end
   
 end
