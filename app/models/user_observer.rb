7c6ddcea866b79335da565dc32acbc87d8ea571a
Roberto Salicio <roberto.salicio@gmail.com>
2008-12-10 18:50:05 +0100

    fix restful-authentication with rails 2.2

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -1,10 +1,16 @@
 class UserObserver < ActiveRecord::Observer
   def after_create(user)
-    UserMailer.deliver_signup_notification(user)
+    UserMailer.deliver_signup_notification(reload_user(user))
   end
 
   def after_save(user)
-    UserMailer.deliver_activation(user) if user.recently_activated?
-    UserMailer.deliver_reset_notification(user) if user.recently_forgot_password?
+    u = reload_user(user)
+    UserMailer.deliver_activation(u) if u.recently_activated?
+    UserMailer.deliver_reset_notification(u) if u.recently_forgot_password?
   end
+
+  private
+    def reload_user(user)
+      User.find(user.id)
+    end
 end
