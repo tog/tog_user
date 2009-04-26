d3d68420af63ed21a2b0b5d9be6b81f1ab8dfa1d
Alberto Molpeceres <alberto.molpeceres@gmail.com>
2009-04-26 15:07:03 +0800

    added a couple of unit test for sign up
    Signed-off-by: tog - extensible open source social network platform <tog@linkingpaths.com>

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,6 +1,49 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class UserTest < ActiveSupport::TestCase
+
+  context "A visitor" do
+
+    context "that signs up" do
+      setup do
+        @chavez = Factory(:user, :login => 'chavez')
+      end
+
+      should "be in state pending" do
+        assert @chavez.state, "pending"
+      end
+          
+      should "generate an activation_code" do
+        assert_not_nil @chavez.activation_code
+      end
+    
+      should "receive an email with a link to activation" do
+        assert_sent_email do |email|
+          email.body.include? @chavez.activation_code
+          email.subject.include?(I18n.t("tog_user.mailer.activate_account"))
+        end
+      end
+      
+      context "that activates his acount" do
+        setup do
+          @chavez.activate!
+        end
+
+        should "be in state active" do
+          assert @chavez.state, "active"
+        end
+
+        should "receive a welcome message" do
+          assert_sent_email do |email|
+            email.subject.include?(I18n.t("tog_user.mailer.account_activated"))
+          end
+        end
+
+      end      
+    
+    end
+  end
+  
   context "A User" do
 
     setup do
